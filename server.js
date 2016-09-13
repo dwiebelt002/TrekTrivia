77e28cfe3502214c49897eb25c2f8aff95068f93
Devin <devinwiebelt@gmail.com>
2016-09-13 13:11:31 -0400

    user auth up and running

--- server.js
+++ server.js
@@ -83,5 +83,5 @@ app.use('/users', users);
 app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'));
+  console.log('Server started on port '+app.get('port'));
 });
